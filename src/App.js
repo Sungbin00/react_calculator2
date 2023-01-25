3b7138b9c7b733aa5f1933f199b0485dbb2e1617
Sungbin00 <ddusin0614@naver.com>
2023-01-26 02:30:39 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='wrapper'>
+        <div className='historyScreen'></div>
+        <div className='screen'></div>
+        <div className='btn light-gray'>%</div>
+        <div className='btn light-gray'>CE</div>
+        <div className='btn light-gray'>C</div>
+        <div className='btn light-gray'>x</div>
+        <div className='btn light-gray'>1/x</div>
+        <div className='btn light-gray'>X2</div>
+        <div className='btn light-gray'>2Vx</div>
+        <div className='btn light-gray'>/</div>
+        <div className='btn'>7</div>
+        <div className='btn'>8</div>
+        <div className='btn'>9</div>
+        <div className='btn light-grey'>X</div>
+        <div className='btn'>4</div>
+        <div className='btn'>5</div>
+        <div className='btn'>6</div>
+        <div className='btn light-gray'>-</div>
+        <div className='btn'>1</div>
+        <div className='btn'>2</div>
+        <div className='btn'>3</div>
+        <div className='btn light-gray'>+</div>
+        <div className='btn'>+/-</div>
+        <div className='btn'>0</div>
+        <div className='btn'>.</div>
+        <div className='btn blue'>=</div>
+      </div>
     </div>
   );
 }
