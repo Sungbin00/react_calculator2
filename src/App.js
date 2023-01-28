cbbc239028744e99d7ce50468b1f362a7d6a9fac
Sungbin00 <ddusin0614@naver.com>
2023-01-28 16:56:48 +0900

    javascript reset기능 추가

--- src/App.js
+++ src/App.js
@@ -2,35 +2,56 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
+const [preState, setPreState] = useState("");
+const [curState, setCurState] = useState("");
+const [input, setInput] = useState("0");
+const [operator, setOperator] = useState(null);
+const [total, setTotal] = useState(false);
+
+// 초기화 함수 계산기의 C 기능
+const allReset = () => {
+  setPreState("");
+  setCurState("");
+  setInput("0");
+};
+
+// 마지막 입력 지우는 함수 계산기의 CE 기능
+const lastReset = () => {
+  setCurState("0");
+}
+
+
   return (
     <div className='container'>
       <div className='wrapper'>
         <div className='historyScreen'></div>
-        <div className='screen'></div>
-        <div className='btn light-gray'>%</div>
-        <div className='btn light-gray'>CE</div>
-        <div className='btn light-gray'>C</div>
-        <div className='btn light-gray'>x</div>
-        <div className='btn light-gray'>1/x</div>
-        <div className='btn light-gray'>X2</div>
-        <div className='btn light-gray'>2Vx</div>
-        <div className='btn light-gray'>/</div>
-        <div className='btn'>7</div>
-        <div className='btn'>8</div>
-        <div className='btn'>9</div>
-        <div className='btn light-grey'>X</div>
-        <div className='btn'>4</div>
-        <div className='btn'>5</div>
-        <div className='btn'>6</div>
-        <div className='btn light-gray'>-</div>
-        <div className='btn'>1</div>
-        <div className='btn'>2</div>
-        <div className='btn'>3</div>
-        <div className='btn light-gray'>+</div>
-        <div className='btn'>+/-</div>
-        <div className='btn'>0</div>
-        <div className='btn'>.</div>
-        <div className='btn blue'>=</div>
+        <div className='screen'>
+          {input}
+        </div>
+        <div className='btn light-gray' onClick={percent}>%</div>
+        <div className='btn light-gray' onClick={lastReset}>CE</div>
+        <div className='btn light-gray' onClick={allReset}>C</div>
+        <div className='btn light-gray' onClick={backspace}>x</div>
+        <div className='btn light-gray' onClick={denominator}>1/x</div>
+        <div className='btn light-gray' onClick={spr}>X2</div>
+        <div className='btn light-gray' onClick={root}>2Vx</div>
+        <div className='btn light-gray' onClick={operatorType}>/</div>
+        <div className='btn' onClick={inputNum}>7</div>
+        <div className='btn' onClick={inputNum}>8</div>
+        <div className='btn' onClick={inputNum}>9</div>
+        <div className='btn light-grey' onClick={operatorType}>X</div>
+        <div className='btn' onClick={inputNum}>4</div>
+        <div className='btn' onClick={inputNum}>5</div>
+        <div className='btn' onClick={inputNum}>6</div>
+        <div className='btn light-gray' onClick={operatorType}>-</div>
+        <div className='btn' onClick={inputNum}>1</div>
+        <div className='btn' onClick={inputNum}>2</div>
+        <div className='btn' onClick={inputNum}>3</div>
+        <div className='btn light-gray' onClick={operatorType}>+</div>
+        <div className='btn' onClick={minusPlus}>+/-</div>
+        <div className='btn' onClick={inputNum}>0</div>
+        <div className='btn' onClick={inputNum}>.</div>
+        <div className='btn blue' onClick={equals}>=</div>
       </div>
     </div>
   );
